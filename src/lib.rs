cc33b0c69e48a89e7418215cfcae7dac385dcbd5
Hitoshi Amano <seijro@gmail.com>
2023-08-12 21:41:19 +0900

    prepare for cargo publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! 
+//! This library helps you detect commonly generated junk files like `.DS_Store` or `Thumbs.db`.
+//! 
+//! # Features
+//! 
+//! 1. Detect common junk files such as `.DS_Store` and `Thumbs.db`.
+//! 2. Auto-update: Uses GitHub Actions to automatically reflect changes from the [original node junk project](https://github.com/sindresorhus/junk) within a week.
+//! 
+//! If you discover new junk files, please consider reporting to the [original junk project](https://github.com/sindresorhus/junk).
+//! 
+//! # Examples
+//! 
+//! ```rust
+//! use junk_file::*;
+//! use std::ffi::OsString;
+//! 
+//! assert_eq!(is_junk("Thumbs.db"), true);
+//! assert_eq!(is_junk(OsString::from(".DS_Store")), true); // also OsStr, OsString acceptable
+//! 
+//! assert_eq!(is_not_junk("filename.txt"), true);
+//! assert_eq!(is_not_junk(OsString::from("filename.txt")), true);
+//!
+//! ```
+
 use regex::Regex;
 use lazy_static::lazy_static;
 use std::ffi::OsStr;
